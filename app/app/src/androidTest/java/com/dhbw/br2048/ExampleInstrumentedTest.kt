a7f5532bb88548eca599832d4e3d35da8ce87a7a
Kai Anter <kai@anter.dev>
2022-02-06 19:16:29 +0100

    Fix: capitalization of app

--- app/app/src/androidTest/java/com/dhbw/br2048/ExampleInstrumentedTest.kt
+++ app/app/src/androidTest/java/com/dhbw/br2048/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dhbw.BR2048", appContext.packageName)
+        assertEquals("com.dhbw.br2048", appContext.packageName)
     }
 }
