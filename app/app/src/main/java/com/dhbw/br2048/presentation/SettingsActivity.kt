08fa55f045a8fb206595d47aaa45b535b1c2f098
Kai Anter <kai@anter.dev>
2022-02-17 12:42:53 +0100

    Added: theme preview grid for theme change

--- app/app/src/main/java/com/dhbw/br2048/presentation/SettingsActivity.kt
+++ app/app/src/main/java/com/dhbw/br2048/presentation/SettingsActivity.kt
@@ -57,6 +57,17 @@ class SettingsActivity : AppCompatActivity() {
             Coordinates(4, 4),
             2,
         )
+        manager.setTile(2, Coordinates(0, 0))
+        manager.setTile(4, Coordinates(1, 0))
+        manager.setTile(8, Coordinates(2, 0))
+        manager.setTile(16, Coordinates(3, 0))
+        manager.setTile(32, Coordinates(0, 1))
+        manager.setTile(64, Coordinates(1, 1))
+        manager.setTile(128, Coordinates(2, 1))
+        manager.setTile(256, Coordinates(3, 1))
+        manager.setTile(512, Coordinates(0, 2))
+        manager.setTile(1024, Coordinates(1, 2))
+        manager.setTile(2048, Coordinates(2, 2))
     }
 
     private fun setCurrentFragment(fragment: Fragment) {
