0c1f39b5bef1fc21a387a580f0fcc06a91e41067
Maximilian Welt <maxi@welt.sh>
2022-03-06 15:02:49 +0100

    Prevent joining empty lobby

--- app/app/src/main/java/com/dhbw/br2048/presentation/LobbyAdapter.kt
+++ app/app/src/main/java/com/dhbw/br2048/presentation/LobbyAdapter.kt
@@ -1,11 +1,14 @@
 package com.dhbw.br2048.presentation
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.dhbw.br2048.R
 import com.dhbw.br2048.data.Lobby
 import com.dhbw.br2048.databinding.ItemLobbyBinding
+import com.google.android.material.snackbar.Snackbar
 
 class LobbyAdapter(private var lobbys: MutableList<Lobby>, private val onClick: (String) -> Unit) : RecyclerView.Adapter<LobbyAdapter.LobbyViewHolder>(){
     inner class LobbyViewHolder(val b: ItemLobbyBinding) : RecyclerView.ViewHolder(b.root)
@@ -24,7 +27,16 @@ class LobbyAdapter(private var lobbys: MutableList<Lobby>, private val onClick: 
         holder.b.lobbyPlayerCount.text = "${l.currentUsers} / ${l.maxUsers}"
 
         holder.itemView.setOnClickListener {
-            onClick(l.id)
+            if(l.owner == "Opening..."){
+                // @todo     android.content.res.Resources$NotFoundException: String resource ID #0x7f1100b6
+                // Snackbar.make(
+                //    holder.b.lobbyID,
+                //    Resources.getSystem().getString(R.string.lobby_opening),
+                //    Snackbar.LENGTH_LONG
+                // ).show()
+            }else{
+                onClick(l.id)
+            }
         }
     }
 
